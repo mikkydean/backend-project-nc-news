ff8ba46b570b5a8a34b5f30052e4a57ed66ba095
Adam Dean <mikkydean@googlemail.com>
2024-04-15 14:28:10 +0100

    Solution and tests for task 4 - CORE: GET /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -42,12 +42,49 @@ describe("/api/topics", () => {
 });
 
 describe("/api", () => {
-    test("GET 200: Should return the updated endpoints.json file with information about avialble endpoints", () => {
+    test("GET 200: Should return the updated endpoints.json file with information about avialable endpoints", () => {
         return request(app)
         .get("/api")
         .expect(200)
         .then((response) => {
-            expect(JSON.parse(response.text)).toEqual(endpoints)
+            expect(JSON.parse(response.text)).toEqual({ endpoints} )
         })
+    })
+})
+
+describe("/api/articles/:article_id", () => {
+    test("GET 200: Should return an article object with the required properties", () => {
+        return request(app)
+        .get("/api/articles/3")
+        .expect(200)
+        .then(({ body }) => {
+            const { article } = body;
+            expect(typeof article.author).toBe("string")
+            expect(typeof article.title).toBe("string")
+            expect(typeof article.article_id).toBe("number")
+            expect(typeof article.body).toBe("string")
+            expect(typeof article.topic).toBe("string")
+            expect(typeof article.created_at).toBe("string")
+            expect(typeof article.votes).toBe("number")
+            expect(typeof article.article_img_url).toBe("string")
+        })
+    })
+    test("GET 404: Should return a 404 'Not found' error if the article ID is not in the database", () => {
+      return request(app)
+      .get("/api/articles/9999")
+      .expect(404)
+      .then(({ body }) => {
+        const { message } = body;
+        expect(message).toBe("Not found")
+      })
+    })
+    test("GET 404: Should return a 400 'Invalid request' error if the article ID is of the incorrect type", () => {
+      return request(app)
+      .get("/api/articles/headlines")
+      .expect(400)
+      .then(({ body }) => {
+        const { message } = body;
+        expect(message).toBe("Invalid request")
+      })
     })
 })
