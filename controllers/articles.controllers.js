408d78037b6f548dd9fde7d3565d33f6584d4e0b
Adam Dean <mikkydean@googlemail.com>
2024-04-16 12:53:00 +0100

    refactoring of controller and model for getArticles as per feedback

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,4 +1,9 @@
-const { selectArticleById, selectArticleIdByCount, selectArticles } = require("../models/articles.models");
+const {
+  selectArticleById,
+  selectArticleIdByCount,
+  selectArticles,
+  selectArticles2
+} = require("../models/articles.models");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
@@ -12,24 +17,9 @@ exports.getArticleById = (req, res, next) => {
 };
 
 exports.getArticles = (req, res, next) => {
-  let articleCommentCount 
-  selectArticleIdByCount().then((body) => {
-    articleCommentCount = body
-  }).then(() => {
-    selectArticles().then((articles) => {
-      for (let i=0; i<articles.length; i++) {
-        for (let j=0; j<articleCommentCount.length; j++) {
-        delete articles[i].body
-        if (articles[i].article_id === articleCommentCount[j].article_id) {
-          articles[i].comment_count = Number(articleCommentCount[j].count)
-        }
-        if (!articles[i].comment_count) {
-          articles[i].comment_count = 0
-        }
-      }   
-    }   
-  res.status(200).send({articles})
-  })
-})
+  selectArticles().then((articles) => {
+    res.status(200).send({ articles })
+  }
+)
 }
 
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,30 +1,45 @@
-const db = require("../db/connection")
+const db = require("../db/connection");
 
 exports.selectArticleById = (article_id) => {
-    return db.query(`SELECT * FROM articles WHERE article_id=$1`, [article_id]).then(({ rows }) => {
-        if ( rows.length === 0 ) {
-            return Promise.reject({ status: 404, message: "Not found"})
-        }
-        return rows[0]
-    })
-}
+  return db
+    .query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, message: "Not found" });
+      }
+      return rows[0];
+    });
+};
 
 exports.selectArticleIdByCount = () => {
-    return db.query(`SELECT article_id, COUNT(*) FROM comments GROUP By article_id;`).then(({ rows }) => {
-        return rows
-    })
-}
+  return db
+    .query(`SELECT article_id, COUNT(*) FROM comments GROUP By article_id;`)
+    .then(({ rows }) => {
+      return rows;
+    });
+};
 
 exports.selectArticles = () => {
-    return db.query(`SELECT * FROM articles ORDER BY created_at DESC`).then(({ rows }) => {
-       return rows
-    })
-}
+    return db
+      .query(
+        `SELECT articles.article_id, title, topic, articles.author, articles.created_at::timestamp, articles.votes, article_img_url, CAST(COUNT(comment_id) AS INT) AS comment_count
+      FROM articles
+      LEFT OUTER JOIN comments ON articles.article_id=comments.article_id
+      GROUP BY articles.article_id ORDER BY articles.created_at DESC
+  ;`)
+      .then(({ rows }) => {
+        return rows;
+      });
+  };
 
 exports.checkArticleExists = (article_id) => {
-    return db.query(`SELECT * FROM articles WHERE article_id=$1`, [article_id]).then(({ rows }) => {
-        if (rows.length === 0) {
-            return Promise.reject({ status: 404, message: "Article ID not found"})
-        }
-    })
-} 
+  return db
+    .query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, message: "Article ID not found" });
+      }
+    });
+};
+
+
