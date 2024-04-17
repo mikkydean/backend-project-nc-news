c2b750b8161d5afad7aed12047a24b51b8dfb3ef
Adam Dean <mikkydean@googlemail.com>
2024-04-17 12:05:02 +0100

    Solution and tests for task 12 - GET /api/articles/:article_id (comment_count)

--- models/articles.models.js
+++ models/articles.models.js
@@ -2,7 +2,13 @@ const db = require("../db/connection");
 
 exports.selectArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
+    .query(`SELECT articles.article_id, title, topic, articles.body, articles.author, articles.created_at::timestamp, articles.votes, article_img_url,
+    CAST(COUNT(comment_id) AS INT) AS comment_count
+    FROM articles 
+    LEFT OUTER JOIN comments ON articles.article_id=comments.article_id
+    WHERE articles.article_id=$1
+    GROUP BY articles.article_id ORDER BY articles.created_at DESC
+    ;`, [article_id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, message: "Not found" });
